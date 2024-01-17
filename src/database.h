2557cfd1014a9b529e490e2021b11035ea79590c
Aditya Ghosh <adityaghosh5025@gmail.com>
2024-01-17 08:26:20 +0530

    add file

---
+++ src/database.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <chrono>
+#include <string>
+
+struct Config {
+    // int uuid;
+    int id{-1};
+    std::string tag{"primary"};
+    std::string programName;
+    std::string configPath;
+};
+
+struct ConfigFiles {
+    // int uuid;
+    int id{-1};
+    int programId{-1};
+    std::string filePath;
+    std::time_t lastModified;
+};
